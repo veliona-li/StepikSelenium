4841a1feb9fbaa860eea029f919f45bc5a80d438
veliona <56954184+veliona-li@users.noreply.github.com>
2024-03-03 12:18:10 +0300

    Create lesson1_6_step7.py
    add first commit and first step

---
+++ lesson1_6_step7.py
@@ -0,0 +1,39 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import time
+import math
+
+
+def calc(x):
+    return str(math.log(abs(12 * math.sin(int(x)))))
+
+
+try:
+    link = "http://suninjuly.github.io/get_attribute.html"
+    browser = webdriver.Chrome()
+    browser.get(link)
+
+    x_element = browser.find_element(By.ID, "treasure")  # находим элемент
+    x = x_element.get_attribute("valuex")   # находим значение атрибута у элемента
+    y = calc(x)   # подставляем значение в формулу
+    y_element = browser.find_element(By.ID, "answer")
+    y_element.send_keys(y)
+
+    option1 = browser.find_element(By.ID, "robotCheckbox")
+    option1.click()
+
+    option2 = browser.find_element(By.ID, "robotsRule")
+    option2.click()
+
+    # Отправляем заполненную форму
+    button = browser.find_element(By.CSS_SELECTOR, "button[type='submit']")
+    button.click()
+
+    # ждем загрузки страницы
+    time.sleep(1)
+
+finally:
+    # ожидание чтобы визуально оценить результаты прохождения скрипта
+    time.sleep(10)
+    # закрываем браузер после всех манипуляций
+    browser.quit()
